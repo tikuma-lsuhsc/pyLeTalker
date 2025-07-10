1056634198977f9c6e0f19d9307658b7845fea31
Takeshi Ikuma (LSUHSC) <tikuma@lsuhsc.edu>
2025-07-10 16:39:40 -0500

    updated `test_smb_vt_area_fun` to be pytest ready

--- tests/test_elements_util.py
+++ tests/test_elements_util.py
@@ -4,18 +4,13 @@ import numpy as np
 def test_smb_vt_area_fun():
 
     # fmt:off
-    # params = [5.755, 7.363, 8.477, 0.951, 0.852, 6.655, 0.543, 17.028, 2.706, 17.099, 2.787]
-    params = [5.755, 7.363, 8.477, 0.951, 0.852, 6.655, 0.543, 17.028, 2.706, 17.099, 2.787, 1.866, 1.153, 0.418, 2.893,14.385 ]
+    #         Ap,    npc,   xc,    Ac,    nca,   Aa,    nain,  xin,    Ain,   xlip,   Alip,  xlar,  Alar,  nlarp, xp,    xa
+    # params = [5.755, 7.363, 8.477, 0.951, 0.852, 6.655, 0.543, 17.028, 2.706, 17.099, 2.787, 1.866, 1.153, 0.418, 2.893, 14.385 ]
+    # params = [5.755, 1.000, 8.477, 0.951, 1.000, 6.655, 1.000, 17.028, 2.706, 17.099, 2.787, 1.866, 1.153, 1.000, 2.893, 14.385 ]
+    params = [5.755, 7.363, 8.477, 0.951, 0.852, 6.655, 0.543, 17.028, 2.706, 17.099, 2.787]
     # fmt:on
 
     func = util.smb_vt_area_fun(*params)
-
     x = np.linspace(-1.0,18.0,1001)
-    print(x.shape,x[0],x[-1])
-    from matplotlib import pyplot as plt
-
-    plt.plot(x,func(x))
-    plt.show()
-
-
-test_smb_vt_area_fun()
+    f = func(x)
+    assert x.shape==f.shape
